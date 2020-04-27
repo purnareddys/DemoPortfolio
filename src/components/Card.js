32bf30b4e7299a1aa9b0855d583c3961db43ff14
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-04-28 02:38:07 +0530

    TheFinalCommit

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => (
-  <motion.div className="Card">
+  <div className="Card">
     <a href={props.url} target="_blank"></a>
     <img src={props.image} />
     <h3>{props.title}</h3>
     <p>{props.text}</p>
-  </motion.div>
+  </div>
 );
 
 export default Card;
